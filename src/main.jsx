2f94b4833dc9ca37ad6c9cdf2bfcfdef393bb214
Numa <contato@numadessas.com.br>
2026-04-14 10:08:03 -0300

    fix: 404 redirect via sessionStorage

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './styles/globals.css'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/wiki/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
